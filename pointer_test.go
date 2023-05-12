69095d18c8a71a019b72593e9d051e3854b3b177
Tom Fleet <tomfleet2018@gmail.com>
2023-05-12 18:33:43 +0100

    Add the Or function to deref a pointer safely (#1)

--- pointer_test.go
+++ pointer_test.go
@@ -6,11 +6,72 @@ import (
 	"github.com/FollowTheProcess/pointer"
 )
 
-func TestHello(t *testing.T) {
-	got := pointer.Hello()
-	want := "Hello pointer"
+func TestOr(t *testing.T) {
+	t.Run("string nil", func(t *testing.T) {
+		var str *string // nil
+		or := "default"
 
-	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
-	}
+		got := pointer.Or(str, or)
+
+		if got != or {
+			t.Errorf("got %q, wanted %q", got, or)
+		}
+	})
+
+	t.Run("string non nil", func(t *testing.T) {
+		str := "hello"
+		or := "default"
+
+		got := pointer.Or(&str, or)
+
+		if got != str {
+			t.Errorf("got %q, wanted %q", got, str)
+		}
+	})
+
+	t.Run("int nil", func(t *testing.T) {
+		var i *int // nil
+		or := 42
+
+		got := pointer.Or(i, or)
+
+		if got != or {
+			t.Errorf("got %d, wanted %d", got, or)
+		}
+	})
+
+	t.Run("int non nil", func(t *testing.T) {
+		i := 12
+		or := 42
+
+		got := pointer.Or(&i, or)
+
+		if got != i {
+			t.Errorf("got %d, wanted %d", got, i)
+		}
+	})
+
+	t.Run("struct nil", func(t *testing.T) {
+		type thing struct{ name string }
+		var x *thing
+		or := thing{name: "default"}
+
+		got := pointer.Or(x, or)
+
+		if got != or {
+			t.Errorf("got %v, wanted %v", got, or)
+		}
+	})
+
+	t.Run("struct non nil", func(t *testing.T) {
+		type thing struct{ name string }
+		x := thing{name: "x"}
+		or := thing{name: "default"}
+
+		got := pointer.Or(&x, or)
+
+		if got != x {
+			t.Errorf("got %v, wanted %v", got, x)
+		}
+	})
 }
