9e184b945594d03c71b510b28c44d21782ef15b9
Tom Fleet <tomfleet2018@gmail.com>
2023-05-12 18:30:17 +0100

    Add the Or function to deref a pointer safely

--- pointer.go
+++ pointer.go
@@ -1,7 +1,10 @@
-// Package pointer is a placeholder for something cool.
+// Package pointer implements a number of helpful and simple tools for working safely and conveniently with pointers.
 package pointer
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello pointer"
+// Or returns the value p is pointing to if it is not nil, else v.
+func Or[T any](p *T, v T) T {
+	if p != nil {
+		return *p
+	}
+	return v
 }
